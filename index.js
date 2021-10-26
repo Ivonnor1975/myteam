0145bfcaf1fd67278758aba72e6c13153ef6e421
Ivon <ivonne@jjf.com>
2021-10-25 20:37:34 -0500

    capture manager data with validatins

--- index.js
+++ index.js
@@ -2,145 +2,73 @@ const inquirer = require('inquirer');
 const generatePage = require('./src/page-template');
 const { writeFile, copyFile } = require('./utils/generate-site');
 
-const promptUser = () => {
+const promptManager = () => {
+  console.log(`
+  =======================
+  Please Built your Team!
+  =======================
+  `);
   return inquirer.prompt([
     {
       type: 'input',
       name: 'name',
-      message: 'What is your name? (Required)',
+      message: "What is the team Manager's Name? (Required)",
       validate: nameInput => {
         if (nameInput) {
           return true;
         } else {
-          console.log('Please enter your name!');
+          console.log("Please enter Manager's name!");
           return false;
         }
       }
     },
     {
-      type: 'input',
-      name: 'github',
-      message: 'Enter your GitHub Username (Required)',
-      validate: githubInput => {
-        if (githubInput) {
-          return true;
-        } else {
-          console.log('Please enter your GitHub username!');
-          return false;
-        }
+      type: 'number',
+      name: 'id',
+      message: "What is the team Manager's Id? (Required)",
+      ...validateNumbers(),
+    },
+    {
+      type: "input",
+      name: "email",
+      message: "What is Manager email address?",
+      validate: (answer) => {
+         const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+         if(!emailRegex.test(answer)) {
+             console.log("You have to provide a valid email address!");
+             return false;
+         }
+         return true;
       }
     },
     {
-      type: 'confirm',
-      name: 'confirmAbout',
-      message: 'Would you like to enter some information about yourself for an "About" section?',
-      default: true
-    },
-    {
-      type: 'input',
-      name: 'about',
-      message: 'Provide some information about yourself:',
-      when: ({ confirmAbout }) => confirmAbout
+      type: 'number',
+      name: 'officenumber',
+      message: "What is the team Manager's office number? (Required)",
+      ...validateNumbers(),
     }
-  ]);
+]);
 };
 
-const promptProject = portfolioData => {
-  console.log(`
-=================
-Add a New Project
-=================
-`);
+//validate numbers
+const validateNumbers = moreValidationChecks => ({
+  validate: input => {
+      if (input === '') {
+          return 'Please provide a valid number greater then 0'
+      }
+      return moreValidationChecks ? moreValidationChecks(input) : true
+  },
+  filter: input => {
+      // clear the invalid input
+      return Number.isNaN(input) || Number(input) <= 0 ? '' : Number(input)
+  },
+})
 
-  // If there's no 'projects' array property, create one
-  if (!portfolioData.projects) {
-    portfolioData.projects = [];
-  }
-  return inquirer
-    .prompt([
-      {
-        type: 'input',
-        name: 'name',
-        message: 'What is the name of your project? (Required)',
-        validate: nameInput => {
-          if (nameInput) {
-            return true;
-          } else {
-            console.log('You need to enter a project name!');
-            return false;
-          }
-        }
-      },
-      {
-        type: 'input',
-        name: 'description',
-        message: 'Provide a description of the project (Required)',
-        validate: descriptionInput => {
-          if (descriptionInput) {
-            return true;
-          } else {
-            console.log('You need to enter a project description!');
-            return false;
-          }
-        }
-      },
-      {
-        type: 'checkbox',
-        name: 'languages',
-        message: 'What did you this project with? (Check all that apply)',
-        choices: ['JavaScript', 'HTML', 'CSS', 'ES6', 'jQuery', 'Bootstrap', 'Node']
-      },
-      {
-        type: 'input',
-        name: 'link',
-        message: 'Enter the GitHub link to your project. (Required)',
-        validate: linkInput => {
-          if (linkInput) {
-            return true;
-          } else {
-            console.log('You need to enter a project GitHub link!');
-            return false;
-          }
-        }
-      },
-      {
-        type: 'confirm',
-        name: 'feature',
-        message: 'Would you like to feature this project?',
-        default: false
-      },
-      {
-        type: 'confirm',
-        name: 'confirmAddProject',
-        message: 'Would you like to enter another project?',
-        default: false
-      }
-    ])
-    .then(projectData => {
-      portfolioData.projects.push(projectData);
-      if (projectData.confirmAddProject) {
-        return promptProject(portfolioData);
-      } else {
-        return portfolioData;
-      }
-    });
+
+// TODO: Create a function to initialize app
+function init() {
+  promptManager().then(promptMember)
+  .catch(err => {console.log(err)});
 };
-
-promptUser()
-  .then(promptProject)
-  .then(portfolioData => {
-    return generatePage(portfolioData);
-  })
-  .then(pageHTML => {
-    return writeFile(pageHTML);
-  })
-  .then(writeFileResponse => {
-    console.log(writeFileResponse);
-    return copyFile();
-  })
-  .then(copyFileResponse => {
-    console.log(copyFileResponse);
-  })
-  .catch(err => {
-    console.log(err);
-  });
+// Function call to initialize app
+init();
